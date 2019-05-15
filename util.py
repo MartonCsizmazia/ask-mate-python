4ad632c7948ca2266d457fb9debf6c4bac6eb4d0
David Korpos <korposdavid@gmail.com>
2019-05-15 14:14:15 +0200

    add unix_date_now to util

--- util.py
+++ util.py
@@ -3,3 +3,8 @@ from datetime import datetime
 
 def unix_date_filter(unix_date):
     return datetime.fromtimestamp(unix_date)
+
+
+def unix_date_now():
+    now = datetime.now().timestamp()
+    return int(now)
