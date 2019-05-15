64a890b00f41d154ab1f28ab58397dd3e33fd37b
David Korpos <korposdavid@gmail.com>
2019-05-15 11:13:22 +0200

    move header lists from connection to data_manager

--- connection.py
+++ connection.py
@@ -1,10 +1,6 @@
 import csv
 
 
-QUESTION_HEADER  = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-ANSWER_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-
-
 def get_data_from_csv(file):
     with open(file, encoding="utf8") as opened_file:
         return [row for row in csv.DictReader(opened_file)]
