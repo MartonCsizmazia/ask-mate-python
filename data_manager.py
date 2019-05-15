529750609b25132ca3c23f0683d7e36b4c73f310
David Korpos <korposdavid@gmail.com>
2019-05-15 11:27:03 +0200

    add get_questions to data_manager

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,14 @@
-
+import connection
 
 QUESTION_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 ANSWER_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
+
+def get_questions(question_id=None):
+    questions = connection.get_data_from_csv()
+    if question_id:
+        for question in questions:
+            if question['id'] == question_id:
+                return question
+            raise ValueError("There is no question with the requested ID")
+    return
