6c8a3de16dd346fcebdfb3179a7bd686942e327f
Daniel Kiraly <danielkiraly@ymail.com>
2019-05-15 11:05:52 +0200

    Implement route for question

--- server.py
+++ server.py
@@ -5,10 +5,15 @@ import data_manager.py
 app = Flask(__name__)
 
 
+@app.route('/question/<question_id>', method='GET')
+def route_question(question_id):
+    question = data_manager.get_questions(question_id)
+    answer = data_manager.get_answers_for_question(question_id)
 
-
-
-
+    return render_template('question.html',
+                           question=question,
+                           question_title='Question',
+                           answer=answer)
 
 
 
