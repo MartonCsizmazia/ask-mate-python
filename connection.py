cb45f745a84b0b6070bcf393f9a01aabafa6cfe7
David Korpos <korposdavid@gmail.com>
2019-05-16 12:57:22 +0200

    add edit_row_in_csv

--- connection.py
+++ connection.py
@@ -29,3 +29,17 @@ def delete_data_from_csv(file, id_to_delete, fieldnames):
         for row in existing_data:
             if row['id'] != id_to_delete:
                 writer.writerow(row)
+
+
+def edit_row_in_csv(file, changed_data, fieldnames):
+    existing_data = get_data_from_csv(file)
+
+    with open(file, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+
+        for row in existing_data:
+            if row['id'] == changed_data['id']:
+                writer.writerow(changed_data)
+            else:
+                writer.writerow(row)
