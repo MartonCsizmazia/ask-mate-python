9c08b5779fb6c485dd250ee578db4556e7b4d417
David Korpos <korposdavid@gmail.com>
2019-05-16 13:40:24 +0200

    add date_to_unix

--- util.py
+++ util.py
@@ -5,6 +5,10 @@ def unix_date_filter(unix_date):
     return datetime.fromtimestamp(unix_date)
 
 
+def date_to_unix(date):
+    return datetime.timestamp(date)
+
+
 def unix_date_now():
     now = datetime.now().timestamp()
     return int(now)
