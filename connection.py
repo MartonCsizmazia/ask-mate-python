c019cfcd3137916bcf5196ab06f02bda5186b0ee
Daniel Kiraly <danielkiraly@ymail.com>
2019-05-27 11:01:24 +0200

    Implement database connection

--- connection.py
+++ connection.py
@@ -1,45 +1,45 @@
-import csv
+import os
+import psycopg2
+import psycopg2.extras
 
 
-def get_data_from_csv(file):
-    with open(file, encoding="utf8") as opened_file:
-        return [row for row in csv.DictReader(opened_file)]
+def get_connection_string():
+    user_name = os.environ.get('PSQL_USER_NAME')
+    password = os.environ.get('PSQL_PASSWORD')
+    host = os.environ.get('PSQL_HOST')
+    database_name = os.environ.get('PSQL_DB_NAME')
+
+    env_variables_defined = user_name and password and host and database_name
+
+    if env_variables_defined:
+        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+            user_name=user_name,
+            password=password,
+            host=host,
+            database_name=database_name
+        )
+    else:
+        raise KeyError('Some necessary environment variable(s) are not defined')
 
 
-def export_data_to_csv(file, new_data, fieldnames):
-    existing_data = get_data_from_csv(file)
-
-    with open(file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for row in existing_data:
-            writer.writerow(row)
-
-        writer.writerow(new_data)
+def open_database():
+    try:
+        connection_string = get_connection_string()
+        connection = psycopg2.connect(connection_string)
+        connection.autocommit = True
+    except psycopg2.DatabaseError as exception:
+        print('Database connection problem')
+        raise exception
+    return connection
 
 
-def delete_data_from_csv(file, id_to_delete, fieldnames):
-    existing_data = get_data_from_csv(file)
+def connection_handler(function):
+    def wrapper(*args, **kwargs):
+        connection = open_database()
+        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        ret_value = function(dict_cur, *args, **kwargs)
+        dict_cur.close()
+        connection.close()
+        return ret_value
 
-    with open(file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for row in existing_data:
-            if row['id'] != id_to_delete:
-                writer.writerow(row)
-
-
-def edit_row_in_csv(file, changed_data, fieldnames):
-    existing_data = get_data_from_csv(file)
-
-    with open(file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for row in existing_data:
-            if row['id'] == changed_data['id']:
-                writer.writerow(changed_data)
-            else:
-                writer.writerow(row)
+    return wrapper
